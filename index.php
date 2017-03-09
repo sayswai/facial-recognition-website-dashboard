d7b53d40bd3c3b3cc52cae86ee977e1af3df45b0
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-03-08 18:17:39 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta httpequiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Computer Vision Face Detection Pipeline</title>
+    
+    <!-- Bootstrap CSS -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Bootstrap Fonts -->
+    
+    
+    <!-- Extra CSS -->
+    
+    
+    <!-- Custom CSS -->
+    
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- Navbar -->
+    
+    <!-- Header -->
+    
+    
+    
+    <!-- jQuery -->
+    <script src="lib/jquery/jquery-3.1.1.min.js"></script>
+    
+    <!-- Bootstrap js -->
+    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+    
+    <!-- Javascript Plugin -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    
+    <!-- Custom js -->
+    <script src="js/view.min.js"></script> <!-- TODO create min.js once finished -->
+  </body>
+</html>
