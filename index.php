985831a4276a021a6e257939ee25755c1534898f
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-03-15 17:35:24 -0700

    View.css Update
    Added content to support navbar, and modified body background color for
    navbar testing

--- index.php
+++ index.php
@@ -3,21 +3,21 @@
     <meta charset="utf-8">
     <meta httpequiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <title>Computer Vision Face Detection Pipeline</title>
-    
+
     <!-- Bootstrap CSS -->
     <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    
+
     <!-- Bootstrap Fonts -->
-    
-    
+
+
     <!-- Extra CSS -->
-    
-    
+
+
     <!-- Custom CSS -->
-    
-    
+    <link href="css/view.css" rel="stylesheet">
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -28,7 +28,7 @@
   <body>
     <!-- Navbar -->
     <!-- TODOS: Add auto-scrolling feature, finish profile link, add links once page is further designed, test for visual design -->
-    <nav id="navbar" class="navbar navbar-toggleable-md navbar-inverse bg-inverse" role="navigation">
+    <nav id="navbar" class="navbar navbar-toggleable-md navbar-inverse" role="navigation">
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -46,28 +46,28 @@
           </li>
           <!--<li class="nav-item disabled"> NOT IMPLEMENTED AND NEEDS FURTHER DESIGN WORK; NEED METHOD FOR DETERMING USER LOGIN STATUS, AND TOGGLE VIIBILITY WHEN LOGGED IN (OFF FOR NEW USERS< ON FOR LOGGED IN USERS)
             <a class="nav-link" href="#">Profile</a>
-          </li> -->   
+          </li> -->
           <li class="nav-item disabled"> <!-- Not yet implemented, therefor disabled for now. Create popup form for account management -->
             <a class="nav-link" href="#">Login or Sign Up</a>
           </li>
         </ul>
       </div>
     </nav>
-    
-    
+
+
     <!-- Header -->
-    
-    
-    
+
+
+
     <!-- jQuery -->
     <script src="lib/jquery/jquery-3.1.1.min.js"></script>
-    
+
     <!-- Bootstrap js -->
     <script src="lib/bootstrap/js/bootstrap.min.js"></script>
-    
+
     <!-- Javascript Plugin -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    
+
     <!-- Custom js -->
     <script src="js/view.min.js"></script> <!-- TODO create min.js once finished -->
   </body>
