7e56fae971b7f3b5596aab84abbe01b01ebe9575
Wai Myint <sayswai@gmail.com>
2017-03-29 14:57:39 -0700

    function isVideo() checks by filename if file is accepted file type

--- php-functions/upload.php
+++ php-functions/upload.php
@@ -4,4 +4,32 @@
  * User: Wai
  * Date: 3/29/2017
  * Time: 1:16 PM
+ * Purpose:
+ *  Uploading video to webserver
  */
+
+/* Global Variables*/
+$vtypes = array("mp4", "mpg", "mov");
+$upload_dir = $_SERVER['DOCUMENT_ROOT'].'\uploads\raw_upload';
+
+
+
+
+/*Functions*/
+/**
+ * Validates whether the inputted file extension is any of the acceptable one listed in $vtypes using filename
+ * @param $x inputted filename
+ * @return bool true if file is any of the extensions listed in $vtypes
+ */
+    function isVideo($x) {
+        global $vtypes;
+        if (in_array(pathinfo($x, PATHINFO_EXTENSION), $vtypes)){
+            return true;
+        }
+        return false;
+    }
+    echo $upload_dir;
+
+
+?>
+
