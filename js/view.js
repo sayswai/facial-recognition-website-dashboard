b9ca9bffdba99933236d68a22c466f297c56f4ab
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-04-03 13:57:06 -0700

    LogForm
    Added small login form, no functionality yet, just there for looks!

--- js/view.js
+++ js/view.js
@@ -2,4 +2,7 @@
   "use strict"; //Ensures no undeclared variables are used (ie width=7; is invalid)
 
   $('#nav').load("html/navbar.html");
+  $('#logFormTrigger').click(function() {
+    $('.logForm').toggle();
+  })
 })(jQuery);
