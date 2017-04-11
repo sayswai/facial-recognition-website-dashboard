f53d8a2d2a88f564dd8a4eee3e386cf2f79d3354
Wai Myint <hsueh@waimyint.com>
2017-04-10 17:34:49 -0700

    vsplit dummy

--- php-functions/vsplit.php
+++ php-functions/vsplit.php
@@ -5,3 +5,15 @@
  * Date: 3/29/2017
  * Time: 1:16 PM
  */
+
+/**file hierachy
+ * -/root
+ * --/uploads
+ * ---/raw_uploads
+ * --/vids
+ * ---/*(each folder contains its own video)*
+ * ----/split_frames
+ * ----/detected_frames*/
+
+
+function
