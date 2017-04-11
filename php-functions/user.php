a3a2ca01271c88a86835a42e6a9b9bceb12e05b6
verygold <richpowerminer@gmail.com>
2017-04-10 17:19:36 -0700

    Sample: User Form + DB_Connection

--- php-functions/user.php
+++ php-functions/user.php
@@ -4,4 +4,54 @@
  * User: BlackLabel
  * Date: 4/10/2017
  * Time: 3:59 PM
+ * User Form + Functions
+ * Username
+ *
+ * FirstName
+ * LastName
+ *
+ * Password
+ *
+ *
  */
+
+
+include 'db_connect.php';
+$conn1 = connect_db("postgres", "1", "CS160");
+
+
+?>
+
+<h1>New User Registration</h1>
+<h2>Welcome! Please sign up below!</h2>
+<form>
+
+    <div class = "user-div>
+        <label for="username">Username:</label>
+        <input type="text" class="user-div" id="username">
+    </div>
+
+    <div class = "fname-div>
+        <label for="fname">First Name:</label>
+    <input type="text" class="fname-div" id="fname">
+    </div>
+
+    <div class = "lname-div>
+        <label for="lname">Last Name:</label>
+    <input type="text" class="lname-div" id="lname">
+    </div>
+
+
+    <div class = "pass-div">
+        <label for="pass">Password:</label>
+        <input type="password" class="pass-div" id="pass">
+    </div>
+
+    <button type="submit" class="regbutt">Register!</button>
+</form>
+
+</form>
+
+
+
+
