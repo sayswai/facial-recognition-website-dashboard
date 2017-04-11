283e5f72be838d9f8faff712738901f744b146ce
Wai Myint <hsueh@waimyint.com>
2017-04-10 17:09:19 -0700

    dummy implmentation of folder architecture for videos

--- php-functions/db_connect.php
+++ php-functions/db_connect.php
@@ -9,14 +9,11 @@
  * Connect to local DB
  */
 
-function connect_db($dbusername, $dbpassword, $dbname, $dbhost = "localhost", $dbport = "5432")
-{
-    //Connection variable above
-    $connection = pg_connect("dbname=$dbname host=$dbhost port=$dbport user=$dbusername password=$dbpassword") or die('Connection failed');
-    return $connection;
-}
+    function connect_db($dbusername, $dbpassword, $dbname, $dbhost = "localhost", $dbport = "5432")
+    {
+        //Connection variable above
+        $connection = pg_connect("dbname=$dbname host=$dbhost port=$dbport user=$dbusername password=$dbpassword") or die('Connection failed');
+        return $connection;
+    }
 
-
-$db = connect_db("postgres", "Heispostgres27", "CS160");
-echo pg_host($db);
 ?>
