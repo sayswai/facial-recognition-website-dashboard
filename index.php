0ef80b493af578df249c65f9d03249bfd7314065
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-04-26 16:48:24 -0700

    final adjustments

--- index.php
+++ index.php
@@ -60,8 +60,8 @@
         <div class="modal-dialogue" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
               <h1 class="modal-title">Log In</h1>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
               <form class="form-horizontal">
