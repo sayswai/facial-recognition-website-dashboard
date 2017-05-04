68abd4ef77ed81c574fcdb784a2cdae6f6a0f628
ben <benliuu@gmail.com>
2017-05-03 22:28:02 -0700

    openface working

--- php-functions/openface.php
+++ php-functions/openface.php
@@ -9,14 +9,22 @@
 /*
  * OpenFace command
  * Usage:  ./OpenFace/build/bin/FaceLandmarkImg -fdir "../videos/" -ofdir "./demo_img/" -oidir "./demo_img/"
+ *
+ * To get sudo to work on apache I had to call sudo visudo and add www-data ALL=NOPASSWD: ALL
+ *
+ * Change this line to where OpenFace FaceLandmarkImg is installed
  * */
-$openFaceCommand = "./OpenFace/build/bin/FaceLandmarkImg";
+$openFaceCommand = "sudo /home/ben/Downloads/OpenFace/build/bin/FaceLandmarkImg";
 
 //Directory of split images
-$splitImgDirectory = "";
+$splitImgDirectory = "../vids/fakeVideo/split_frames/";
 
 //Directory where detected landmarks should be written
-$ofDir = "";
+$ofDir = "../vids/fakeVideo/detected_frames/";
+
+$test = 'sudo /home/ben/Downloads/OpenFace/build/bin/FaceLandmarkImg -fdir "../vids/fakeVideo/split_frames/" -ofdir "../vids/fakeVideo/detected_frames/" -q 2>&1';
+;
+
 
 /*
  * Call OpenFace to detected points and save the points into a directory
@@ -25,8 +33,9 @@ function openFace($splitImgDirectory, $ofDir){
 
     global $openFaceCommand;
 
-    $result = shell_exec($openFaceCommand . " -fdir " . "\"". $splitImgDirectory ."\"" . "-ofdir" . "\"" . $ofDir . "\"");
+    $result = shell_exec($openFaceCommand . " -fdir " . '"'. $splitImgDirectory . '"' . " -ofdir " . '"' . $ofDir . '" -q 2>&1');
 
+	printf($results);
 }
 
 /*
@@ -39,3 +48,7 @@ function writePoint($ptsFile){
     fclose($myfile);
 
 }
+
+openFace($splitImgDirectory, $ofDir);
+
+
