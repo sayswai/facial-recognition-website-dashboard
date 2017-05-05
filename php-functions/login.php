eb09c0296d61bdd81fa9b24cc05270c59aa0a55d
verygold <richpowerminer@gmail.com>
2017-05-04 17:30:45 -0700

    Edits

--- php-functions/login.php
+++ php-functions/login.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 include 'db_connect.php';
 //Database Connection to Postgresql.
 $conn1 = connect_db("postgres", "1", "CS160");
@@ -13,24 +13,32 @@ else{
     echo "The connection is invalid.";
 }
 
+
 class auth
 {
+    function nounce(){
+        $randomchars = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789!@#$%^&*()_+{}|:?>[]\?<"';
+        $charstrlen = strlen($randomchars);
+        $output = '';
+        //GENERATE THE RANDOM CHARS TO OUTPUT
+        for ($a=0; $a<10; $a++) {
+            $output .= $randomchars[rand(0,$charstrlen-1)];
+        }
+        return $output;
+    }
+
     function access()
-    {
-        if (!empty($_POST)) {
+    { $usernounce = $this->nounce();
+        if (isset($_POST)) {
             $usernamez = filter_input(INPUT_POST, 'userr', FILTER_SANITIZE_STRING);
             $passwordz = filter_input(INPUT_POST, 'passs', FILTER_SANITIZE_STRING);
-            $encrypt = 'BitchImBadANDBoujieWhipUpTheDopeWithAnUZI';
             $trimmedp = trim($passwordz);
-            $PASS = crypt($trimmedp, $encrypt);
-
-
-            session_start();
-
-            $my_name = $_SESSION['User'] = $usernamez;
-
+            $PASS0 = hash('sha512',$trimmedp);
+            $PASS = trim($PASS0);
             $query = "SELECT * FROM users";
             $result = pg_query($query) or die ("Cannot execute query :$query\n");
+
+
 
             //LEST SEE IF ANY MATCHES IN DATABASE
 
@@ -38,31 +46,27 @@ class auth
             while($col = pg_fetch_row($result))
             {
                 //checks both user & pass
-                if (strcasecmp(trim($usernamez),trim($col[1])) == 0)
+                if (strcasecmp(trim($usernamez),trim($col[1])) == 0) //check if username matches user in the database
                 {
-                    $i = 3;
+                    $i = 100;
                     if (trim($col[2]) == $PASS) //check password that matches user
                     {
                         $i = 1;
+                        $_SESSION['username'] = $usernamez;
                     }
                 }
             }
             //statements
             switch ($i)
             {
-                case 1:header("Location: upload.php");
-                    break;
-
-                case 2:echo "The Username entered has not been registered, please register";
-                    break;
-
-                default:echo "Invalid Login, please try again";
-                    break;
+                case 1:header("Location: upload.php");break; //GOOD!
+                case 2:echo "Username not found, please register if you haven't!";break;
+                default:echo "Invalid Authentication!";break;
             }
-
         }
     }
 }
 $grantyes = new auth();
+$grantyes->nounce();
 $grantyes->access();
 ?>
