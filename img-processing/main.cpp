f44b169dbb5fccc055de4ef383d291aede3209f8
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-05-11 22:51:06 -0700

    Compilable
    main.cpp in img-processing can now be compiled with only loss of
    precision errors from necessary casting

--- img-processing/main.cpp
+++ img-processing/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <fstream>
 #include <libpq-fe.h>
+#include <unistd.h>
 
 
 const int qcount = 1440; //Number of times to attempt each query (qcount*slplen=max_execution_time)
@@ -20,33 +21,33 @@ static void connection_exit(PGconn *pgconn){
 }
 
 //Util: Draw a point
-static void dot(Mat& image, Point2f ref){
-  Scalar color = Scalar(223, 61, 130);
+static void dot(cv::Mat& image, cv::Point2f ref){
+  cv::Scalar color = cv::Scalar(223, 61, 130);
   circle(image, ref, 2, color, CV_FILLED, CV_AA, 0);
 }
 
 //Util: Draw all triangles
-static void triangles(Mat& image, Subdiv2D& sdiv, int width, int height){
-  Scalar color = Scalar(223, 61, 130);
+static void triangles(cv::Mat& image, cv::Subdiv2D& sdiv, int width, int height){
+  cv::Scalar color = cv::Scalar(223, 61, 130);
 
   //Get list of triangles to draw from sdiv into triangles, and define workspace
-  vector<Vec6f> triangles;
-  vector<Point> tripoints(3);
+  std::vector<cv::Vec6f> triangles;
+  std::vector<cv::Point> tripoints;
   sdiv.getTriangleList(triangles);
-  Rect space = Rect(0,0,width,height);
+  cv::Rect space = cv::Rect(0,0,width,height);
 
   //Draw every triangle
   for(int i=0; i<triangles.size(); i++){
-    Vec6f tri = triangles[i];
-    tripoints[0] = Point(cvRound(tri[0]), cvRound(tri[1]));
-    tripoints[1] = Point(cvRound(tri[2]), cvRound(tri[3]));
-    tripoints[2] = Point(cvRound(tri[4]), cvRound(tri[5]));
+    cv::Vec6f tri = triangles[i];
+    tripoints[0] = cv::Point(cvRound(tri[0]), cvRound(tri[1]));
+    tripoints[1] = cv::Point(cvRound(tri[2]), cvRound(tri[3]));
+    tripoints[2] = cv::Point(cvRound(tri[4]), cvRound(tri[5]));
 
     //Ensure triangles are inside image
     if(space.contains(tripoints[0]) && space.contains(tripoints[1]) && space.contains(tripoints[2])){
-      line(img, tripoints[0], tripoints[1], color, 1, CV_AA, 0);
-      line(img, tripoints[1], tripoints[2], color, 1, CV_AA, 0);
-      line(img, tripoints[2], tripoints[0], color, 1, CV_AA, 0);
+      cv::line(image, tripoints[0], tripoints[1], color, 1, CV_AA, 0);
+      cv::line(image, tripoints[1], tripoints[2], color, 1, CV_AA, 0);
+      cv::line(image, tripoints[2], tripoints[0], color, 1, CV_AA, 0);
     }
   }
 }
@@ -55,13 +56,15 @@ int main( int argc, const char** argv ){
   const char *vID = argv[1];
 
   const char *pginfo;
-  const char *pg_vidquery;
+  char *pg_vidquery;
   PGconn *pgconn;
   PGresult *pgres;
 
   //DB connect and query info
   pginfo = "dbname=CS160 host=localhost port=5432 user=postgres password=1";
-  pg_vidquery = "SELECT framecount, width, height FROM (SELECT * FROM videos WHERE vid = "+ vID +")";
+  strcpy(pg_vidquery, "SELECT framecount, width, height FROM (SELECT * FROM videos WHERE vid = ");
+  strcat(pg_vidquery, vID);
+  strcat(pg_vidquery, ")");
 
 
   //Start and test connection
@@ -80,31 +83,32 @@ int main( int argc, const char** argv ){
   }
 
   //Get values from result, and clear result
-  const int fcount = static_cast<int>(PQgetvalue(pgres, 0, 0));
-  const int width = static_cast<int>(PQgetvalue(pgres, 0, 1));
-  const int height = static_cast<int>(PQgetvalue(pgres, 0, 2));
+  const int fcount = (int)(PQgetvalue(pgres, 0, 0));
+  const int width = (int)(PQgetvalue(pgres, 0, 1));
+  const int height = (int)(PQgetvalue(pgres, 0, 2));
   PQclear(pgres);
 
   for(int i=1; i<=fcount; i++){
     PGresult *pgres2;
 
     //Make i easily usable
-    std::ostringstream oss;
-    oss << i;
-    char *fnum = oss.str();
+    char *fnum = (char)(i);
 
     //Set up query for current frame number
     char *pg_ofquery;
     char *pg_iquery;
-    pg_ofquery = "SELECT * FROM openface WHERE framenum = " + fnum;
-    pg_iquery = "SELECT rightpupilx, rightpupily, leftpupilx, leftpupily FROM (SELECT * FROM eye WHERE framenum = "+ fnum +")";
+    strcpy(pg_ofquery, "SELECT * FROM openface WHERE framenum = ");
+    strcat(pg_ofquery, fnum);
+    strcpy(pg_iquery, "SELECT rightpupilx, rightpupily, leftpupilx, leftpupily FROM (SELECT * FROM eye WHERE framenum = ");
+    strcat(pg_iquery, fnum);
+    strcat(pg_iquery, ")");
 
     //Execute queries, wait up to a day if current frame not stored in either database table
     pgres = PQexec(pgconn, pg_ofquery);
     int c = 0;
     while(PQresultStatus(pgres) != PGRES_TUPLES_OK && c<qcount){
       PQclear(pgres);
-      sleep(slplen);
+      usleep(slplen);
       pgres = PQexec(pgconn, pg_ofquery);
       c++;
     }
@@ -118,7 +122,7 @@ int main( int argc, const char** argv ){
     c = 0;
     while(PQresultStatus(pgres2) != PGRES_TUPLES_OK && c<qcount){
       PQclear(pgres2);
-      sleep(slplen);
+      usleep(slplen);
       pgres2 = PQexec(pgconn, pg_iquery);
       c++;
     }
@@ -130,43 +134,68 @@ int main( int argc, const char** argv ){
 
     //Create padded frame number for file navigation
     char *nav;
-    switch(fnum){
-      case (fnum<10):
-        nav = "000"+fnum;
-      case (fnum<100):
-        nav = "00"+fnum;
-      case (fnum<1000):
-        nav = "0"+fnum;
-      default:
-        nav = fnum;
+    if(i<10){
+      strcpy(nav, "000");
+      strcat(nav, fnum);
+    }
+    else if(i<100){
+      strcpy(nav, "00");
+      strcat(nav, fnum);
+    }
+    else if(i < 1000){
+      strcpy(nav, "0");
+      strcat(nav, fnum);
+    }
+    else {
+      strcpy(nav, fnum);
     }
 
     //Get image and define space to partition into triangles
-    Mat img_original = imread("/var/www/html/vids/"+ vID +"/split_"+ nav +".png");
-    Rect space = Rect(0,0,width,height);
+    char *imgurl;
+    strcpy(imgurl, "/var/www/html/vids/");
+    strcat(imgurl, vID);
+    strcat(imgurl, "/split_");
+    strcat(imgurl, nav);
+    strcat(imgurl, ".png");
+    cv::Mat img_original = cv::imread(imgurl);
+    cv::Rect space = cv::Rect(0,0,width,height);
 
     //Get pupil x and y, and draw dots on them
-    Point2f pupilRight = Point2f(PQgetvalue(pgres2, 0, 0), PQgetvalue(pgres2, 0, 1));
-    Point2f pupilLeft = Point2f(PQgetvalue(pgres2, 0, 2), PQgetvalue(pgres2, 0, 3));
+    std::string rnx(PQgetvalue(pgres2, 0, 0));
+    std::string rny(PQgetvalue(pgres2, 0, 1));
+    std::string lnx(PQgetvalue(pgres2, 0, 2));
+    std::string lny(PQgetvalue(pgres2, 0, 3));
+    float rx = std::stof(rnx);
+    float ry = std::stof(rny);
+    float lx = std::stof(lnx);
+    float ly = std::stof(lny);
+    cv::Point2f pupilRight = cv::Point2f(rx, ry);
+    cv::Point2f pupilLeft = cv::Point2f(lx, ly);
     dot(img_original, pupilRight);
     dot(img_original, pupilLeft);
 
     //Create subdiv2d with area defined above
-    sdiv = Subdiv2D subdiv(space);
+    cv::Subdiv2D sdiv = cv::Subdiv2D(space);
 
     //Extract points from result of openface Query and store in sdiv. Draw dot on image for each point
-    vector<Point2f> points;
+    std::vector<cv::Point2f> points;
     for(int j=0; j<PQnfields(pgres); j++){
-      Point2f p = Point2f(PQgetvalue(pgres, 0, j)[0], PQgetvalue(pgres, 0, j)[1]);
+      cv::Point2f p = cv::Point2f(PQgetvalue(pgres, 0, j)[0], PQgetvalue(pgres, 0, j)[1]);
       sdiv.insert(p);
       dot(img_original, p);
     }
 
     //Draw triangles on image
-    triangles(img, sdiv, width, height);
+    triangles(img_original, sdiv, width, height);
 
     //Write image to new file
-    imwrite("/var/www/html/vids/" + vID +"/detected_frames/" + fnum + ".png");
+    char *url;
+    strcpy(url, "/var/www/html/vids/");
+    strcat(url, vID);
+    strcat(url, "/detected_frames/");
+    strcat(url, fnum);
+    strcat(url, ".png");
+    cv::imwrite(url, img_original);
 
   }
   PQfinish(pgconn);
