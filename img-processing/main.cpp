6f2d5dd536aa772b186f4b86593b68a08aa5210e
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-05-14 01:08:16 -0700

    argv update

--- img-processing/main.cpp
+++ img-processing/main.cpp
@@ -55,7 +55,7 @@ static void triangles(cv::Mat& image, cv::Subdiv2D& sdiv, int width, int height)
   }
 }
 
-int main( int argc, const char** argv ){
+int main( int argc, char* argv[] ){
   const char *vID = argv[1];
 
   const char *pginfo;
