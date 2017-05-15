ebb9f4d3a7e448ad3f0ead0043283968f34b81e3
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-05-15 00:45:00 -0700

    loop attempt

--- img-processing/main.cpp
+++ img-processing/main.cpp
@@ -82,7 +82,11 @@ int main(int argc, char* argv[]){
   printf("Variables prepared for DB connection\n");
 
   //Start and test connection
-  pgconn = PQconnectdb(pginfo);
+  while(PQstatus(pgconn) != CONNECTION_OK){
+    try{
+      pgconn = PQconnectdb(pginfo);
+    }
+  }
   if(PQstatus(pgconn) != CONNECTION_OK){
     printf("Connection to postgres failed: %s\n", PQerrorMessage(pgconn));
     connection_exit(pgconn);
