3b3b081bd0ae53552cb6adc833250128a91ee044
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-05-14 22:17:25 -0700

    print point to look at it

--- img-processing/main.cpp
+++ img-processing/main.cpp
@@ -173,7 +173,8 @@ int main(int argc, char* argv[]){
   //Extract points from result of openface Query and store in sdiv. Draw dot on image for each point
   std::vector<cv::Point2f> points;
   for(int j=0; j<PQnfields(pgres); j++){
-    cv::Point2f p = cv::Point2f(PQgetvalue(pgres, 0, j));
+    printf("PQvalue: %s", PQgetvalue(pgres, 0, j));
+    cv::Point2f p = cv::Point2f(PQgetvalue(pgres, 0, j)[0], PQgetvalue(pgres, 0, j)[1]);
     sdiv.insert(p);
     dot(img_original, p);
   }
