ea3e107ef99b1c73fb9396e1e03bcb52e9d0890e
sayswai <sayswai@gmail.com>
2017-05-15 23:45:21 -0700

    I FOUND THE PROBLEM , THERE WERE DUPLICATES STORED INTO THE DATABASE FOR VIDEO VISUAL POINTS

--- php-functions/video_render_functions.php
+++ php-functions/video_render_functions.php
@@ -188,12 +188,16 @@ function openCv($vID){
 	$det_frame = 1;
 	shell_exec('sudo chown -R www-data:www-data ' .$VID_DIR. '');
 	shell_exec('sudo mkdir -p '.$VID_DIR.'finished_frames/');
+	echo 'count - '.count($arr).'<br>';
 	for($i = 2; $i < sizeof($splitFiles); $i++) {
 		$frame = $i-1;
-		if($arr[($det_frame-1)]['framenum'] == $frame && ($det_frame - 1) < (count($arr)-1)){
+		if($arr[($det_frame-1)]['framenum'] == $frame && (($det_frame - 1) < (count($arr)-1))){
 			//frame is detected in both openface and eyelike
-			shell_exec('sudo '.$openCvCommand.' '.$vID.' '.$frame);
+			shell_exec($openCvCommand.' '.$vID.' '.$frame);
 			$det_frame++;
+			if ($arr[($det_frame - 1)]['framenum'] == $arr[($det_frame - 2)]['framenum']){
+				$det_frame++;
+			}
 		}else{
 			//frame is not detected, just move over file form split to finished_frame folder
 			$outputName = str_replace('split_', 'output_', $splitFiles[$i]);
